5db4e689c3f54942df64836e1f373df9ba721b6a
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2024-11-24 15:44:42 +0200

    a

--- ex-1-external/react-micro-frontend-tutorial/packages/store/config/webpack.dev.js
+++ ex-1-external/react-micro-frontend-tutorial/packages/store/config/webpack.dev.js
@@ -1,26 +1,26 @@
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
 
 const devConfig = {
-  mode: 'development',
+  mode: "development",
   devServer: {
     port: 8081,
     historyApiFallback: {
-      index: 'index.html',
+      index: "index.html",
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'store',
-      filename: 'remoteEntry.js',
+      name: "store",
+      filename: "remoteEntry.js",
       exposes: {
-        './StoreApp': './src/bootstrap',
+        "./StoreApp": "./src/bootstrap",
       },
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
   ],
 };
